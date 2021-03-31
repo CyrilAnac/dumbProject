62d8de7e09777cfebb3b32f31251ebe38ca63939
Cyril A <cyril.anaclet@gmail.com>
2021-03-31 11:25:34 +0200

    🗑 Suppression code inutilisé

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,23 +1,10 @@
 import * as React from "react";
-import {
-  View,
-  LinearGradient,
-  StyleSheet,
-  Header,
-  Image,
-  Button,
-  Text,
-} from "react-native";
-import {
-  createStackNavigator,
-  HeaderBackButton,
-} from "@react-navigation/stack";
+import { Image, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Recherche from "../components/pages/Recherche";
 import Home from "../components/pages/Home";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
 import Ordonnance from "../components/pages/Ordonnance";
-import Icon from "react-native-elements";
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -57,21 +44,6 @@ function RootHome() {
           },
         }}
       />
-      <Stack.Screen name="Drawer" component={MyDrawer} />
-      <Stack.Screen //ce screen la contient la page Ordonnance + son en-tete que je défini juste en dessous
-        name="Ordonnance"
-        component={Ordonnance}
-        options={{
-          headerTitle: () => (
-            <Text style={{ color: "#FFFFFF", fontSize: 20 }}>
-              Mon Ordonnance
-            </Text>
-          ),
-          headerStyle: {
-            backgroundColor: "#FFAE74",
-          },
-        }}
-      />
     </Stack.Navigator>
   );
 }
@@ -103,21 +75,6 @@ function MyDrawer() {
       <Drawer.Screen name="Accueil" component={RootHome} />
       <Drawer.Screen name="Informations Personnelle" component={Recherche} />
       <Drawer.Screen name="Mon Ordonnance" component={RootOrdonnance} />
-      {/*<Drawer.Screen
-        name="Mon Ordonnance"
-        component={Ordonnance}
-        options={{
-          headerTitle: () => (
-            <Text style={{ color: "#FFFFFF", fontSize: 20 }}>
-              Mon Ordonnance
-            </Text>
-          ),
-          headerStyle: {
-            backgroundColor: "#FFAE74",
-          },
-        }}
-      />
-      {/*et je veux appeler le screen ici pour avoir Onrdonnance.js + son entete*/}
       <Drawer.Screen name="Espace Bien-être" component={RootHome} />
       <Drawer.Screen name="Conditions d'Utilisations" component={RootHome} />
       <Drawer.Screen name="Mentions Légales" component={RootHome} />
@@ -125,71 +82,5 @@ function MyDrawer() {
     </Drawer.Navigator>
   );
 }
-/*
-function Navigation() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerTitle: (props) => <LogoTitle {...props} />,
-          headerStyle: {
-            backgroundColor: "#FFAE74",
-            //header: props => <GradientHeader {...props} />,
-          },
-        }}
-      />
-      <Stack.Screen
-        name="Recherche"
-        component={Recherche}
-        options={{
-          headerTitle: () => (
-            <Text style={{ color: "#FFFFFF", fontSize: 20 }}>Recherche</Text>
-          ),
-          headerStyle: {
-            backgroundColor: "#FFAE74",
-          },
-        }}
-      />
-    </Stack.Navigator>
-  );
-}
-
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Informations Personnelles" component={Home} />
-      <Drawer.Screen name="Mon Ordonnance" component={Recherche} />
-      <Drawer.Screen name="Espace Bien-être" component={Recherche} />
-      <Drawer.Screen name="Conditions d'Utilisations" component={Recherche} />
-      <Drawer.Screen name="Mentions Légales" component={Recherche} />
-      <Drawer.Screen name="Aide" component={Recherche} />
-    </Drawer.Navigator>
-  );
-};
-*/
-
-/* TODO Gradiant toolbar 
-const GradientHeader = props => (
-  <View style={{ backgroundColor: '#eee' }}>
-      <LinearGradient
-        colors={['red', 'blue']}
-        style={{height: Header.HEIGHT }}>
-        <Header {...props} />
-      </LinearGradient>
-    </View>
-  )*/
-const styles = StyleSheet.create({
-  containerNav: {
-    backgroundColor: "#FFFFFF",
-    alignItems: "center",
-  },
-  logo: {
-    width: 10,
-    height: 10,
-  },
-});
 
 export default MyDrawer;
