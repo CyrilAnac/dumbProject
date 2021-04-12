f55a357d49e3bcffb7f876801f4be821105310b9
Thomas WALOSIK <thomas.walosik@gmail.com>
2021-04-12 17:38:22 +0200

    Importations des images fiches cancer

--- components/pages/FichesCancer.js
+++ components/pages/FichesCancer.js
@@ -32,112 +32,191 @@ export default function FichesCancer({ navigation }) {
       <ScrollView
         horizontal={false}
         showsVerticalScrollIndicator={true}
-        style={{}}
+        style={{ marginLeft: 10 }}
       >
-        {/*<View style={styles.container}>*/}
         <Text style={styles.titre}>Édition COMPRENDRE ET AGIR</Text>
-        <TouchableOpacity onPress={() => alert("SEIN")} style={{}}>
-          <Image
-            source={require("../../assets/item_ficheSEIN.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert("PEAU")} style={{}}>
-          <Image
-            source={require("../../assets/item_fichePEAU.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            marginTop: 20,
+          }}
+        >
+          <TouchableOpacity onPress={() => alert("Fiche SEIN")}>
+            <Image
+              source={require("../../assets/SEIN2.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert("LEUCEMIES")} style={{}}>
-          <Image
-            source={require("../../assets/item_ficheLEUCEMIES.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert("Fiche PEAU")}>
+            <Image
+              source={require("../../assets/PEAU2.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 10 }}>
+          <TouchableOpacity onPress={() => alert("Fiche LEUCEMIES")}>
+            <Image
+              source={require("../../assets/LEUCEMIES2.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert("PROSTATE")} style={{}}>
-          <Image
-            source={require("../../assets/item_fichePROSTATE.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert("Fiche PROSTATE")}>
+            <Image
+              source={require("../../assets/PROSTATE2.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 10 }}>
+          <TouchableOpacity onPress={() => alert("Fiche ESTOMAC")}>
+            <Image
+              source={require("../../assets/ESTOMAC.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert("REIN")} style={{}}>
-          <Image
-            source={require("../../assets/item_ficheREIN.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert("Fiche COLORECTAL")}>
+            <Image
+              source={require("../../assets/COLORECTAL.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 10 }}>
+          <TouchableOpacity onPress={() => alert("Fiche FOIE")}>
+            <Image
+              source={require("../../assets/FOIE.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => alert("COLORECTAL")} style={{}}>
-          <Image
-            source={require("../../assets/item_ficheCOLORECTAL.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => alert("TESTICULE")} style={{}}>
-          <Image
-            source={require("../../assets/item_ficheTESTICULE.png")}
-            resizeMethod="contain"
-            style={{
-              width: "100%",
-              height: "120%",
-              resizeMode: "cover",
-              alignSelf: "center",
-              borderWidth: 2,
-              borderRadius: 20,
-            }}
-          />
-        </TouchableOpacity>
+          <TouchableOpacity onPress={() => alert("Fiche OVAIRE")}>
+            <Image
+              source={require("../../assets/OVAIRE.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 10 }}>
+          <TouchableOpacity onPress={() => alert("Fiche PANCREAS")}>
+            <Image
+              source={require("../../assets/PANCREAS.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => alert("Fiche POUMON")}>
+            <Image
+              source={require("../../assets/POUMON.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: "row", marginTop: 10 }}>
+          <TouchableOpacity onPress={() => alert("Fiche REIN")}>
+            <Image
+              source={require("../../assets/REIN.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => alert("Fiche TESTICULE")}>
+            <Image
+              source={require("../../assets/TESTICULE.jpg")}
+              resizeMode="contain"
+              style={styles.imageInfoCancer}
+            />
+          </TouchableOpacity>
+        </View>
+        {/*
+      <TouchableOpacity onPress={() => alert("LEUCEMIES")} style={{}}>
+        <Image
+          source={require("../../assets/item_ficheLEUCEMIES.png")}
+          resizeMethod="contain"
+          style={{
+            width: "100%",
+            height: "120%",
+            resizeMode: "cover",
+            alignSelf: "center",
+            borderWidth: 2,
+            borderRadius: 20,
+          }}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => alert("PROSTATE")} style={{}}>
+        <Image
+          source={require("../../assets/item_fichePROSTATE.png")}
+          resizeMethod="contain"
+          style={{
+            width: "100%",
+            height: "120%",
+            resizeMode: "cover",
+            alignSelf: "center",
+            borderWidth: 2,
+            borderRadius: 20,
+          }}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => alert("REIN")} style={{}}>
+        <Image
+          source={require("../../assets/item_ficheREIN.png")}
+          resizeMethod="contain"
+          style={{
+            width: "100%",
+            height: "120%",
+            resizeMode: "cover",
+            alignSelf: "center",
+            borderWidth: 2,
+            borderRadius: 20,
+          }}
+        />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => alert("COLORECTAL")} style={{}}>
+        <Image
+          source={require("../../assets/item_ficheCOLORECTAL.png")}
+          resizeMethod="contain"
+          style={{
+            width: "100%",
+            height: "120%",
+            resizeMode: "cover",
+            alignSelf: "center",
+            borderWidth: 2,
+            borderRadius: 20,
+          }}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => alert("TESTICULE")} style={{}}>
+        <Image
+          source={require("../../assets/item_ficheTESTICULE.png")}
+          resizeMethod="contain"
+          style={{
+            width: "100%",
+            height: "120%",
+            resizeMode: "cover",
+            alignSelf: "center",
+            borderWidth: 2,
+            borderRadius: 20,
+          }}
+        />
+      </TouchableOpacity>*/}
       </ScrollView>
     </View>
   );
@@ -151,7 +230,7 @@ function ItemCancer(props) {
     >
       <View>
         <Image
-          source={require("../../assets/IMG_3759.jpg")}
+          source={require("../../assets/SEIN.jpg")}
           resizeMode="contain"
           style={styles.imageInfoCancer}
         />
@@ -168,7 +247,20 @@ const styles = StyleSheet.create({
   },
   titre: {
     fontSize: 20,
-    marginBottom: 20,
+    marginTop: 20,
     alignItems: "center",
+  },
+  fiche: {
+    width: "100%",
+    height: "110%",
+    alignSelf: "center",
+    resizeMode: "contain",
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  imageInfoCancer: {
+    width: 162,
+    height: 184,
+    marginLeft: 17,
   },
 });
